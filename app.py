175f4762211a8859cb024f1537e3287c2a4912fe
Nivetha2405 <niv24ha@gmail.com>
2026-02-02 15:33:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,3 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for("login"))
 
-# ------------------- Run App -------------------
-if __name__ == "__main__":
-    app.run(debug=True)
